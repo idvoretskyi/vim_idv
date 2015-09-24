130f987de22bb0777cd6df85ad3d88e6c4a3e2ea
Ihor Dvoretskyi <igor.dvoretskyi@gmail.com>
2015-09-24 12:47:28 +0300

    vimrc update

--- install.sh
+++ install.sh
@@ -4,10 +4,9 @@ cat .vimrc > ~/.vimrc
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
  
-cp -r .vim/plugins.vim ~/.vim/plugins.vim
-cp -r .vim/keybindings.vim ~/.vim/keybindings.vim
+cat .vim/plugins.vim > ~/.vim/plugins.vim
+cat .vim/keybindings.vim > ~/.vim/keybindings.vim
 
 vim +PluginInstall +qall
-
 
 echo "Vim configuration installed successfully!"
