2025468f2ddb26ed867c7032822d0bfdc116ff32
Ihor Dvoretskyi <ihor.dvoretskyi@gmail.com>
2016-01-11 02:02:30 +0200

    Updated version

--- install.sh
+++ install.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
+# Cloning the repo
 git clone https://github.com/idvoretskyi/awesome_vimrc.git ~/vimrc
 
+# Installing awesome VIM distribution
 cd ~/vimrc
-
 cat .vimrc > ~/.vimrc
-
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
- 
 cat .vim/plugins.vim > ~/.vim/plugins.vim
 cat .vim/keybindings.vim > ~/.vim/keybindings.vim
-
 vim +PluginInstall +qall
 
 echo "Vim configuration installed successfully!"
