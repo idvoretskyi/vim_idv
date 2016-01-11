9c24aa356f6a1f74131e1c4f22c7aa4b1bd65de0
Ihor Dvoretskyi <ihor.dvoretskyi@gmail.com>
2016-01-12 00:22:31 +0200

    name convention update

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Cloning the repo
-git clone https://github.com/idvoretskyi/vim_idv.git ~/vimrc
+git clone https://github.com/idvoretskyi/vim_idv.git ~/vim_idv
 
 # Installing awesome VIM distribution
-ln -s ~/vimrc/.vimrc ~/.vimrc
-ln -s ~/vimrc/.vim ~/.vim
+ln -s ~/vim_idv/vimrc ~/.vimrc
+ln -s ~/vim_idv/vim ~/.vim
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
 
