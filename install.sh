f972fccf048aef9c2f0746908e63a1240345393f
Ihor Dvoretskyi <ihor.dvoretskyi@gmail.com>
2016-01-12 22:13:24 +0200

    minor fixes

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ VIMDIR="~/vim_idv"
 # Cloning the repo
 if [ -e "$VIMDIR" ]; then
     echo "This VIM configuration has been already installed. Please, run the
-    update script for updating it"
+    update script for updating it" && exit 0
 else
     git clone https://github.com/idvoretskyi/vim_idv.git $VIMDIR
 fi
