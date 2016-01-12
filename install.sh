919019b0084790432c68e815b1d96d89bb659801
Ihor Dvoretskyi <ihor.dvoretskyi@gmail.com>
2016-01-12 18:28:32 +0200

    minor fix

--- install.sh
+++ install.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
+VIMDIR="~/vim_idv/"
+
 # Cloning the repo
-git clone https://github.com/idvoretskyi/vim_idv.git ~/vim_idv/
+git clone https://github.com/idvoretskyi/vim_idv.git $VIMDIR 
 
 # Installing awesome VIM distribution
-ln -s ~/vim_idv/vimrc ~/.vimrc
-ln -s ~/vim_idv/vim ~/.vim
+ln -s $VIMDIR/vimrc ~/.vimrc
+ln -s $VIMDIR/vim ~/.vim
 
-cd ~/.vim/
+cd $VIMDIR/vim
 git clone https://github.com/VundleVim/Vundle.vim.git bundle/Vundle.vim
 vim +PluginInstall +qall
 
