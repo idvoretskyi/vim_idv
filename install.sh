64eb30d9f354437bae4ac8bf98ed37100429c70e
Ihor Dvoretskyi <ihor.dvoretskyi@gmail.com>
2016-05-02 22:12:49 -0700

    minor fix

--- install.sh
+++ install.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
-VIMDIR="$HOME/vim_idv"
+VIMDIR="$HOME/.vim_idv"
+
+# Installing awesome VIM distribution
+if [ -e $HOME/.vimrc ];then
+    rm -rf $HOME/.vimrc
+fi
+
+if [ -e $HOME/.vim ];then
+    rm -rf $HOME/.vim
+fi
+
+if [ -e $VIMDIR ];then
+    rm -rf $VIMDIR
+fi
 
 # Cloning the repo
 git clone https://github.com/idvoretskyi/vim_idv.git $VIMDIR
-
-# Installing awesome VIM distribution
-if [ -e $HOME/.vim ];then
-    rm -rf $HOME/.vim*
-fi
 
 ln -s $VIMDIR/vimrc $HOME/.vimrc && ln -s $VIMDIR/functions $HOME/.vim
 
