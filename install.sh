bb39a19708d9a059653028dfd0ac4ec49efc5c2e
Ihor Dvoretskyi <ihor@linux.com>
2017-12-09 20:57:22 +0000

    Configuration updated

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-VIMDIR="$HOME/.vim_idv"
+VIMDIR="$HOME/.vim_runtime"
 
-# Installing awesome VIM distribution
+# Installing the awesome VIM distribution
 if [ -e $HOME/.vimrc ];then
     rm -rf $HOME/.vimrc
 fi
@@ -13,16 +13,10 @@ fi
 
 if [ -e $VIMDIR ];then
     rm -rf $VIMDIR
-fi
-
-# Cloning the repo
-git clone https://github.com/idvoretskyi/vim_idv.git $VIMDIR
+f
 
 ln -s $VIMDIR/vimrc $HOME/.vimrc && ln -s $VIMDIR/functions $HOME/.vim
 
 cd $HOME/.vim
 git clone https://github.com/VundleVim/Vundle.vim.git bundle/Vundle.vim
-git clone git://github.com/altercation/vim-colors-solarized.git bundle/vim-colors-solarized
 vim +PluginInstall +qall
-
-clear && echo "VIM configuration installed successfully!"
